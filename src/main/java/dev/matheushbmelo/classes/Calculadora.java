f456ed4fd83674af10f5291e61895a7f9d997643
MatheusHBMelo <matheushbmelo@gmail.com>
2023-10-02 11:04:22 -0300

    Realizando teste manual de um metodo sem Junit

---
+++ src/main/java/dev/matheushbmelo/classes/Calculadora.java
@@ -0,0 +1,14 @@
+package dev.matheushbmelo.classes;
+
+public class Calculadora {
+    public int soma(int a, int b){
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        System.out.println(calc.soma(2, 2) == 4);
+        System.out.println(calc.soma(5, 6) == 10);
+        System.out.println(calc.soma(1, 8) == 9);
+    }
+}
