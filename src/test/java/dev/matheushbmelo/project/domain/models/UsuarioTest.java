6989a9bac22a7bcf3d15f09588369e09d5aab02c
MatheusHBMelo <matheushbmelo@gmail.com>
2023-10-17 10:11:30 -0300

    Update

--- src/test/java/dev/matheushbmelo/project/domain/models/UsuarioTest.java
+++ src/test/java/dev/matheushbmelo/project/domain/models/UsuarioTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static dev.matheushbmelo.project.domain.models.builders.UsuarioBuilder.umUsuario;
@@ -60,6 +61,7 @@ public class UsuarioTest {
         assertEquals("A senha não pode ser nula!", ex.getMessage());
     }
 
+    // Os três testes anteriores com parameterized test
     @ParameterizedTest(name = "[{0}] - {4}")
     @CsvSource(value = {
             "1, NULL, user@email.com, 12345, O nome não pode ser nulo!",
@@ -67,6 +69,16 @@ public class UsuarioTest {
             "3, usuario, user@email.com, NULL, A senha não pode ser nula!"
     }, nullValues = "NULL")
     public void verificarCamposObrigatoriosDoUsuario(Long id, String nome, String email, String senha, String mensagem) {
+        ValidationException ex = Assertions.assertThrows(ValidationException.class,
+                () -> umUsuario().comId(id).comNome(nome).comEmail(email).comSenha(senha).agora()
+        );
+        assertEquals(mensagem, ex.getMessage());
+    }
+
+    // O parameterized test com dados externos
+    @ParameterizedTest(name = "[{0}] - {4}")
+    @CsvFileSource(files = "src\\camposObrigatoriosUsuario.csv", nullValues = "NULL", numLinesToSkip = 1)
+    public void verificarCamposObrigatorioDoUsuarioComArquivoExterno(Long id, String nome, String email, String senha, String mensagem){
         ValidationException ex = Assertions.assertThrows(ValidationException.class,
                 () -> umUsuario().comId(id).comNome(nome).comEmail(email).comSenha(senha).agora()
         );
